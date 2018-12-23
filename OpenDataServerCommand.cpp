6380daf323e07b4ef15da08c588c9f8829a113f5
sharonbash26 <45743653+sharonbash26@users.noreply.github.com>
2018-12-23 18:07:23 +0200

    Add files via upload

--- OpenDataServerCommand.cpp
+++ OpenDataServerCommand.cpp
@@ -1,8 +1,87 @@
-//
-// Created by sharon_i4 on 18/12/2018.
-//
-
+#include <iostream>
 #include "OpenDataServerCommand.h"
-int OpenDataServerCommand::execute(){
-    return 3;
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+
+int OpenDataServerCommand::execute() {
+    int i = 0;
+    unsigned long lenBindMap = bindMap.size();
+    unsigned long lenVarMap = varMap.size();
+    for (i = 0; i < lenVarMap; i++) {
+        this->openServer(varMap[bindedVariables[i]], bindMap[bindedVariables[i]]);
+    }
+}
+
+
+int OpenDataServerCommand::openServer(std::string, std::string link) {
+    //this is from code of modle server.
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    /* First call to socket() function */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(this->port);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    /* Now bind the host address using bind() call.*/
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        throw 0;//the bind to socket failed
+    }
+
+    /* Now start listening for the clients, here process will
+       * go in sleep mode and will wait for the incoming connection
+    */
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    /* Accept actual connection from the client */
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
+
+    if (newsockfd < 0) {
+        perror("ERROR on accept");
+        throw 0;
+    }
+    while (true) {
+        /* If connection is established then start communicating */
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            throw 0;
+        }
+
+        printf("Here is the message: %s\n", buffer);
+
+        /* Write a response to the client */
+        n = write(newsockfd, "I got your message", 18);
+
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+
+        return 0;
+
+    }
 }
--- OpenDataServerCommand.h
+++ OpenDataServerCommand.h
@@ -1,12 +1,32 @@
 #ifndef OPEN_DATA_SERVER_COMMAND_H
 #define OPEN_DATA_SERVER_COMMAND_H
 
+#include <string>
+#include <map>
+#include <vector>
 #include "Command.h"
 
 class OpenDataServerCommand : public Command
 {
+    std::string port ;
+    std::map <std::string,std::string> varMap;
+    std::map <std::string, std::string> bindMap;
+    std::vector<std::string> bindedVariables;
+
 public:
-    int execute() override ;
+    int execute();
+    void getFirstArgument(std::string arg){this->port = arg;}
+    void getSecondArgument(std::string arg){}
+    void getThirdArgument(std::string arg){}
+    void setVarsAndBinds(std::map <std::string, std::string> _varMap,
+                         std::map <std::string, std::string> _bindMap,
+                         std::vector<std::string> _bindedVariables){
+        varMap = _varMap;
+        bindMap = _bindMap;
+        bindedVariables = _bindedVariables;
+    }
+    int openServer(std::string,std::string link) ;
 };
+
 
 #endif
